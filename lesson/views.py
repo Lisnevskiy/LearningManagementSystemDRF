79014766b1bbc2b85faf20844d944e640b69de0f
Alexander <Lisnevskiy14@gmail.com>
2023-09-06 01:09:26 +0300

    Added Lesson views

--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from lesson.models import Lesson
+from lesson.serializers import LessonSerializer
+
+
+class LessonListView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDetailView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonCreateView(generics.CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDeleteView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
