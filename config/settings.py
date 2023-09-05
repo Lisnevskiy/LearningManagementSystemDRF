65ada7b5054c59e51e6e6f0f1d733be7fc6f9316
Alexander <Lisnevskiy14@gmail.com>
2023-09-05 22:42:03 +0300

    Update project settings

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_URL = "/users/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
