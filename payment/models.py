dbb5d6c577072b8fae1d54aca0f77db17723b0a1
Alexander <Lisnevskiy14@gmail.com>
2023-09-08 15:06:29 +0300

    Added Payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from constants import NULLABLE
+from course.models import Course
+from lesson.models import Lesson
+from users.models import User
 
-# Create your models here.
+
+class Payment(models.Model):
+    """
+    Модель для хранения информации о платежах пользователей за курсы или уроки.
+    """
+
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='payment',  # Обратная связь для пользователя
+        verbose_name='пользователь'
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,  # Устанавливаем поле course в None при удалении курса
+        **NULLABLE,
+        related_name='payment',  # Обратная связь для курса
+        verbose_name='оплаченный курс'
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,  # Устанавливаем поле lesson в None при удалении урока
+        **NULLABLE,
+        related_name='payment',  # Обратная связь для урока
+        verbose_name='оплаченный урок'
+    )
+    payment_date = models.DateField(auto_now_add=True, verbose_name='дата оплаты')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f"Платеж от {self.user}"
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
