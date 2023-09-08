6675debfd30e5be96a1242d66287518231a80625
Alexander <Lisnevskiy14@gmail.com>
2023-09-08 11:58:53 +0300

    Added foreign key to Lesson model

--- lesson/models.py
+++ lesson/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from constants import NULLABLE
+from course.models import Course
 
 
 class Lesson(models.Model):
@@ -9,6 +10,8 @@ class Lesson(models.Model):
     image = models.ImageField(upload_to='lesson/', verbose_name='изображение', **NULLABLE)
     video_url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
     # video_link = models.CharField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', related_name='lesson')
 
     def __str__(self):
         return self.title
