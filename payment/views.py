55289e91ea79f0cbbde907d7810ab4fb86ea742d
Alexander <Lisnevskiy14@gmail.com>
2023-09-08 22:48:35 +0300

    Added Payment views

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
+
+
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
