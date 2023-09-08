c7ac67905a848b5c97740d5b8eb6b27330110343
Alexander <Lisnevskiy14@gmail.com>
2023-09-08 23:28:46 +0300

    Update LessonSerializer

--- lesson/serializers.py
+++ lesson/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
+# from rest_framework.relations import SlugRelatedField
 
+# from course.models import Course
 from lesson.models import Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
 
+    # SlugRelatedField используется для представления поля course как строки (title),
+    # а не как объекта Course, и запрос для queryset настроен на Course.objects.all()
+    # course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+
     class Meta:
         model = Lesson
-        fields = ('pk', 'title', 'description', 'image', 'video_url')
+        fields = ('pk', 'title', 'description', 'image', 'video_url', 'course')
