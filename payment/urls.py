9bcf04ce788e4bb9fc82eee4b64320294188349b
Alexander <Lisnevskiy14@gmail.com>
2023-09-08 22:48:00 +0300

    Added Payment urls

---
+++ payment/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from payment.apps import PaymentConfig
+from payment.views import *
+
+app_name = PaymentConfig.name
+
+urlpatterns = [
+    path('', PaymentListView.as_view(), name='payments'),
+    path('<int:pk>/', PaymentDetailView.as_view(), name='payments_detail'),
+    # path('create/', PaymentCreateView.as_view(), name='payments_create'),
+    # path('update/<int:pk>/', PaymentUpdateView.as_view(), name='payments_update'),
+    # path('delete/<int:pk>/', PaymentDeleteView.as_view(), name='payments_delete'),
+]
