a27696bbd138ce567c5f51326a125fecfa81d248
Alexander <Lisnevskiy14@gmail.com>
2023-09-13 12:14:23 +0300

    Views closed without authorization

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,9 @@ LOGIN_URL = "/users/"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import UserSerializer, UserDetailSerializer, UserRegisterSerializer
@@ -19,6 +20,7 @@ class UserCreateView(generics.CreateAPIView):
     Представление для создания нового пользователя.
     """
     serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         """
