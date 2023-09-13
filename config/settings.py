750e8d9b952b0eaf0311b9a47b1487f933c3b56b
Alexander <Lisnevskiy14@gmail.com>
2023-09-13 11:53:20 +0300

    Added JWT authorization

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,12 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/users/"
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
