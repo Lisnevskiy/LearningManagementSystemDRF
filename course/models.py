0b2bfa24a5c4e18ae7a3f555335b76e5b701e375
Alexander <Lisnevskiy14@gmail.com>
2023-09-15 16:25:01 +0300

    Update Course model

--- course/models.py
+++ course/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 from constants import NULLABLE
+from users.models import User
 
 
 class Course(models.Model):
     title = models.CharField(max_length=300, verbose_name='название')
     description = models.TextField(verbose_name='содержание')
     image = models.ImageField(upload_to='course/', verbose_name='изображение', **NULLABLE)
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец')
 
     def __str__(self):
         return self.title
