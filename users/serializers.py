96afc0ecbd590e8619e30ecfd31adad9266952f3
Alexander <Lisnevskiy14@gmail.com>
2023-09-15 16:24:33 +0300

    Update User serializers

--- users/serializers.py
+++ users/serializers.py
@@ -22,13 +22,21 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('pk', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar')
 
 
-class UserDetailSerializer(serializers.ModelSerializer):
+class UserOwnerDetailSerializer(serializers.ModelSerializer):
     """
-    Сериализатор для модели User, предназначенный для вывода подробной информации о пользователе,
-    включая информацию о платежах пользователя.
+    Сериализатор для детальной информации о пользователе для владельца профиля.
     """
     payment = PaymentSerializer(many=True, read_only=True)  # Сериализатор для поля payment, включая множество платежей
 
     class Meta:
         model = User
         fields = ('pk', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payment')
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для детальной информации о пользователе для других пользователей.
+    """
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'phone', 'city', 'avatar')
