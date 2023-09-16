8bee2443f3c35f5a005ed802cbf869fa969781b2
Alexander <Lisnevskiy14@gmail.com>
2023-09-17 02:14:07 +0300

    Update Course views

--- course/views.py
+++ course/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from course.models import Course
 from course.serializers import CourseSerializer, CourseDetailSerializer
@@ -34,7 +35,7 @@ class CourseViewSet(viewsets.ModelViewSet):
         Метод определяет, какие разрешения применять в зависимости от действия в представлении.
         """
         if self.action in ['list', 'retrieve']:
-            permission_classes = [IsOwner | IsStaff]
+            permission_classes = [AllowAny]
         else:
             permission_classes = [IsOwner]
 
