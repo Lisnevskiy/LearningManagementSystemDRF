cc6ff028a6b2fb509c0f1e70f2f9f60c7a520e0d
Alexander <Lisnevskiy14@gmail.com>
2023-09-17 02:12:26 +0300

    Added Subscription views

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from subscription.models import Subscription
+from subscription.serializers import SubscriptionSerializer
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+    def perform_create(self, serializer):
+
+        subscription = serializer.save()
+        subscription.user = self.request.user
+        subscription.save()
